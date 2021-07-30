f068edd436ee7b6ceafa8817aa88e260d7fda5dd
Nick <nickeast1998@gmail.com>
2021-07-30 10:37:23 +0100

    final changes to blogs

--- src/components/sections/blogSection.js
+++ src/components/sections/blogSection.js
@@ -8,234 +8,236 @@ import Image from '../functional/Image'
 import TwinkleIcon from '../../svgs/shines.svg'
 
 const BlogSectionStyles = styled.section`
-    width: 100%;
-    padding: 2rem 0;
-    background-color: var(--offWhite);
-    .b-s {
-        width: 90%;
-        margin: var(--auto);
-        max-width: var(--maxWidth);
-        &__featured {
-        }
-        &__grid {
-            margin-top: 1rem;
-            display: grid;
-            grid-template-columns: repeat(1, 1fr);
-            grid-gap: 2rem;
-            @media only screen and (min-width: 768px) {
-                grid-template-columns: repeat(3, 1fr);
-                grid-gap: 1rem;
-            }
-        }
-        span {
-            a {
-                button {
-                    width: 150px;
-                    margin-top: 2rem;
-                }
-            }
-        }
+ width: 100%;
+ padding: 2rem 0;
+ background-color: var(--offWhite);
+ .b-s {
+  width: 90%;
+  margin: var(--auto);
+  max-width: var(--maxWidth);
+  &__featured {
+  }
+  &__grid {
+   margin-top: 1rem;
+   display: grid;
+   grid-template-columns: repeat(1, 1fr);
+   grid-gap: 2rem;
+   @media only screen and (min-width: 768px) {
+    margin-top: 1rem;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 1rem;
+   }
+   @media only screen and (min-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr);
+    margin-top: 1.5rem;
+   }
+  }
+  span {
+   a {
+    button {
+     width: 150px;
+     margin-top: 2rem;
     }
+   }
+  }
+ }
 `
 const BlogSection = ({ feaBlog, filteredBlogs }) => {
-    const i = true
-    return (
-        <BlogSectionStyles>
-            <div className="b-s">
-                <div className="b-s__featured">
-                    <FeaturedBlog data={feaBlog} />
-                </div>
-                <div className="b-s__grid">
-                    {filteredBlogs.map((blog) => (
-                        <SingleBlog data={blog} key={blog.id} />
-                    ))}
-                </div>
-                <span>
-                    <Link to="/all-blogs">
-                        <button type="button" className="btn btn--main">
-                            <span>View more</span>
-                        </button>
-                    </Link>
-                </span>
-            </div>
-        </BlogSectionStyles>
-    )
+ const i = true
+ return (
+  <BlogSectionStyles>
+   <div className="b-s">
+    <div className="b-s__featured">
+     <FeaturedBlog data={feaBlog} />
+    </div>
+    <div className="b-s__grid">
+     {filteredBlogs.map((blog) => (
+      <SingleBlog data={blog} key={blog.id} />
+     ))}
+    </div>
+    <span>
+     <Link to="/all-blogs">
+      <button type="button" className="btn btn--main">
+       <span>View more</span>
+      </button>
+     </Link>
+    </span>
+   </div>
+  </BlogSectionStyles>
+ )
 }
 
 BlogSection.propTypes = {
-    feaBlog: PropTypes.any,
-    filteredBlogs: PropTypes.any,
+ feaBlog: PropTypes.any,
+ filteredBlogs: PropTypes.any,
 }
 
 export default BlogSection
 
 const FeaturedBlogStyles = styled.article`
+ width: 100%;
+ height: 100%;
+ .fea {
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  border: transparent 2px solid;
+  border-radius: 8.5px;
+  transition: all 0.15s ease-in-out;
+  &:hover {
+   border: solid #600eff 2px;
+   transform: translateY(-10px);
+  }
+  &__img {
+   position: relative;
+   width: 100%;
+   height: 100%;
+   .gatsby-image-wrapper {
     width: 100%;
-    .fea {
-        box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
-        border: transparent 2px solid;
-        border-radius: 8.5px;
-        transition: all 0.15s ease-in-out;
-        &:hover {
-            border: solid #600eff 2px;
-            transform: translateY(-10px);
-        }
-        &__img {
-            position: relative;
-            width: 100%;
-            height: 100%;
-            .gatsby-image-wrapper {
-                width: 100%;
-                height: 250px;
-                border-radius: 8.5px 8.5px 0 0;
-                @media only screen and (min-width: 768px) {
-                    border-radius: 8.5px 0 0 8.5px;
-                    height: 100%;
-                }
-            }
-            .overlay {
-                position: absolute;
-                top: 0.5rem;
-                left: 1rem;
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-                background-color: #600eff;
-                border: solid 2px #151417;
-                border-radius: 4.5px;
-                padding: 0.45rem;
-                width: 110px;
-                svg {
-                    width: 20px;
-                    height: 20px;
-                }
-                p {
-                    color: var(--white);
-                }
-                @media only screen and (min-width: 414px) {
-                    width: 120px;
-                }
-            }
-        }
-        &__main {
-            background-color: var(--white);
-            border-radius: 0 0 8.5px 8.5px;
-            padding: 1rem;
-            h4 {
-                font-weight: 700;
-                font-size: 1rem;
-            }
-            h2 {
-                margin: 0.5rem 0;
-                font-size: 2rem;
-                font-weight: 500;
-            }
-            > p {
-                font-weight: 700;
-                color: var(--secondary);
-            }
-            &__author {
-                border-top: solid 1px rgba(0, 0, 0, 0.1);
-                margin-top: 1rem;
-                padding: 1rem 0 0.5rem 0.25rem;
-                display: flex;
-                align-items: center;
-                section {
-                    position: relative;
-                    .gatsby-image-wrapper {
-                        border-radius: 5px;
-                        width: 50px;
-                        height: 50px;
-                        border: solid 3px #151417;
-                        z-index: 99;
-                    }
-                    .overlay {
-                        position: absolute;
-                        width: 100%;
-                        height: 100%;
-                        content: '';
-                        bottom: -5px;
-                        left: -5px;
-                        background-color: var(--secondary);
-                        border: solid 3px #151417;
-                        border-radius: 5px;
-                        z-index: 4;
-                    }
-                }
-                .info {
-                    margin-left: 15px;
-                    h6 {
-                        font-size: 1.2rem;
-                    }
-                    p {
-                        font-weight: 700;
-                    }
-                }
-                @media only screen and (min-width: 768px) {
-                    padding-top: 2rem;
-                }
-            }
-            @media only screen and (min-width: 768px) {
-                border-radius: 0 8.5px 8.5px 0;
-                padding-top: 2rem;
-            }
-            /* padding: 1.5rem 2rem; */
-        }
-        @media only screen and (min-width: 768px) {
-            a {
-                height: 16.5rem;
-                display: grid;
-                grid-template-columns: 45% 1fr;
-            }
-        }
+    height: 250px;
+    border-radius: 8.5px 8.5px 0 0;
+    @media only screen and (min-width: 768px) {
+     border-radius: 8.5px 0 0 8.5px;
+     height: 100%;
     }
+   }
+   .overlay {
+    position: absolute;
+    top: 0.5rem;
+    left: 0.5rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #600eff;
+    border: solid 2px #151417;
+    border-radius: 4.5px;
+    padding: 0.45rem;
+    svg {
+     width: 20px;
+     height: 20px;
+    }
+    p {
+     color: var(--white);
+     padding-left: 7.5px;
+    }
+    @media only screen and (min-width: 414px) {
+    }
+   }
+  }
+  &__main {
+   background-color: var(--white);
+   border-radius: 0 0 8.5px 8.5px;
+   padding: 1rem;
+   h4 {
+    font-weight: 700;
+    font-size: 1rem;
+    color: var(--secondary);
+   }
+   h2 {
+    margin: 0.5rem 0;
+    font-size: 1.75rem;
+    font-weight: 500;
+   }
+   > p {
+    font-weight: 700;
+    color: var(--secondary);
+   }
+   &__author {
+    border-top: solid 1px rgba(0, 0, 0, 0.1);
+    margin-top: 1rem;
+    padding: 1rem 0 0.5rem 0.25rem;
+    display: flex;
+    align-items: center;
+    section {
+     position: relative;
+     .gatsby-image-wrapper {
+      border-radius: 5px;
+      width: 50px;
+      height: 50px;
+      border: solid 3px #151417;
+      z-index: 99;
+     }
+     .overlay {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      content: '';
+      bottom: -5px;
+      left: -5px;
+      background-color: var(--secondary);
+      border: solid 3px #151417;
+      border-radius: 5px;
+      z-index: 4;
+     }
+    }
+    .info {
+     margin-left: 15px;
+     h6 {
+      font-size: 1.2rem;
+     }
+     p {
+      font-weight: 700;
+     }
+    }
+    @media only screen and (min-width: 768px) {
+     padding-top: 2rem;
+    }
+   }
+   @media only screen and (min-width: 768px) {
+    border-radius: 0 8.5px 8.5px 0;
+    padding-top: 2rem;
+   }
+   /* padding: 1.5rem 2rem; */
+  }
+  @media only screen and (min-width: 768px) {
+   a {
+    display: grid;
+    grid-template-columns: 45% 1fr;
+   }
+  }
+ }
 `
 const FeaturedBlog = ({ data }) => {
-    const { author } = data.authors[0]
-    const image = getImage(author.image.asset)
-    return (
-        <FeaturedBlogStyles>
-            <div className="fea">
-                <Link to={data.slug.current}>
-                    <div className="fea__img">
-                        <Image data={data} />
-                        <div className="overlay">
-                            <TwinkleIcon />
-                            <p>Featured</p>
-                        </div>
-                    </div>
-                    <div className="fea__main">
-                        <h4>Blog</h4>
-                        <h2>{data.title}</h2>
-                        <p>{`${data.publishedAt} | ${data.readingTimeInMinutes} min read`}</p>
-                        <section className="fea__main__author">
-                            <section>
-                                <GatsbyImage
-                                    image={image}
-                                    alt={author.image.alt}
-                                />
-                                <div className="overlay" />
-                            </section>
-                            <div className="info">
-                                <h6>{author.name}</h6>
-                                <p>{author.position}</p>
-                            </div>
-                        </section>
-                    </div>
-                </Link>
-            </div>
-        </FeaturedBlogStyles>
-    )
+ const { author } = data.authors[0]
+ const image = getImage(author.image.asset)
+ return (
+  <FeaturedBlogStyles>
+   <div className="fea">
+    <Link to={data.slug.current}>
+     <div className="fea__img">
+      <Image data={data} />
+      <div className="overlay">
+       <TwinkleIcon />
+       <p>Featured</p>
+      </div>
+     </div>
+     <div className="fea__main">
+      <h4>Blog</h4>
+      <h2>{data.title}</h2>
+      <p>{`${data.publishedAt} | ${data.readingTimeInMinutes} min read`}</p>
+      <section className="fea__main__author">
+       <section>
+        <GatsbyImage image={image} alt={author.image.alt} />
+        <div className="overlay" />
+       </section>
+       <div className="info">
+        <h6>{author.name}</h6>
+        <p>{author.position}</p>
+       </div>
+      </section>
+     </div>
+    </Link>
+   </div>
+  </FeaturedBlogStyles>
+ )
 }
 
 FeaturedBlog.propTypes = {
-    data: PropTypes.shape({
-        authors: PropTypes.any,
-        publishedAt: PropTypes.any,
-        readingTimeInMinutes: PropTypes.any,
-        slug: PropTypes.shape({
-            current: PropTypes.any,
-        }),
-        title: PropTypes.any,
-    }),
+ data: PropTypes.shape({
+  authors: PropTypes.any,
+  publishedAt: PropTypes.any,
+  readingTimeInMinutes: PropTypes.any,
+  slug: PropTypes.shape({
+   current: PropTypes.any,
+  }),
+  title: PropTypes.any,
+ }),
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -9,130 +9,127 @@ import BlogCTA from '../components/sections/blogCTA'
 import Testimonial from '../components/sections/Testimonial'
 
 const Blog = ({ data }) => {
-    const allBlogs = data.blogs.nodes
-    const featuredBlog = data.featuredBlog.nodes[0]
-    return (
-        <Layout alt>
-            <BlogHeader
-                title="Blog"
-                description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-            />
-            <BlogSection feaBlog={featuredBlog} filteredBlogs={allBlogs} />
-            <Testimonial
-                quote="Becca was really supportive and helpful, meeting up with me regularly to answer any questions and give advice, but also allowing me to have autonomy of the franchise"
-                name="Rachel Drew"
-                location="Dramabuds, Coulsdon"
-            >
-                <StaticImage
-                    src="../assets/images/rachel.jpg"
-                    alt="Georgia Thompson | Dramabuds Wandsworth "
-                    placeholder="blurred"
-                />
-            </Testimonial>
-            <BlogCTA />
-        </Layout>
-    )
+ const allBlogs = data.blogs.nodes
+ const featuredBlog = data.featuredBlog.nodes[0]
+ return (
+  <Layout alt>
+   <BlogHeader
+    title="Blog"
+    description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+   />
+   <BlogSection feaBlog={featuredBlog} filteredBlogs={allBlogs} />
+   <Testimonial
+    quote="Becca was really supportive and helpful, meeting up with me regularly to answer any questions and give advice, but also allowing me to have autonomy of the franchise"
+    name="Rachel Drew"
+    location="Dramabuds, Coulsdon"
+   >
+    <StaticImage
+     src="../assets/images/rachel.jpg"
+     alt="Georgia Thompson | Dramabuds Wandsworth "
+     placeholder="blurred"
+    />
+   </Testimonial>
+   <BlogCTA />
+  </Layout>
+ )
 }
 
 Blog.propTypes = {
-    data: PropTypes.shape({
-        blogs: PropTypes.shape({
-            nodes: PropTypes.any,
-        }),
-        featuredBlog: PropTypes.shape({
-            nodes: PropTypes.any,
-        }),
-        nodes: PropTypes.any,
-    }),
+ data: PropTypes.shape({
+  blogs: PropTypes.shape({
+   nodes: PropTypes.any,
+  }),
+  featuredBlog: PropTypes.shape({
+   nodes: PropTypes.any,
+  }),
+  nodes: PropTypes.any,
+ }),
 }
 
 export default Blog
 
 export const blogQuery = graphql`
-    query BlogQuery {
-        blogs: allSanityPost(
-            sort: { fields: publishedAt, order: DESC }
-            skip: 1
-        ) {
-            nodes {
-                title
-                id
-                excerpt
-                readingTimeInMinutes
-                slug {
-                    current
-                }
-                publishedAt(formatString: "MMM DD YYYY")
-                _rawBody
-                authors {
-                    author {
-                        name
-                        position
-                        image {
-                            alt
-                            asset {
-                                gatsbyImageData(
-                                    width: 200
-                                    placeholder: BLURRED
-                                    formats: [AUTO, WEBP, AVIF]
-                                )
-                            }
-                        }
-                    }
-                }
-                mainImage {
-                    alt
-                    asset {
-                        gatsbyImageData(
-                            width: 500
-                            placeholder: BLURRED
-                            formats: [AUTO, WEBP, AVIF]
-                        )
-                    }
-                }
-            }
-        }
-        featuredBlog: allSanityPost(
-            sort: { fields: publishedAt, order: DESC }
-            limit: 1
-        ) {
-            nodes {
-                title
-                id
-                excerpt
-                readingTimeInMinutes
-                slug {
-                    current
-                }
-                publishedAt(formatString: "MMM DD YYYY")
-                _rawBody
-                authors {
-                    author {
-                        name
-                        position
-                        image {
-                            alt
-                            asset {
-                                gatsbyImageData(
-                                    width: 400
-                                    placeholder: BLURRED
-                                    formats: [AUTO, WEBP, AVIF]
-                                )
-                            }
-                        }
-                    }
-                }
-                mainImage {
-                    alt
-                    asset {
-                        gatsbyImageData(
-                            width: 400
-                            placeholder: BLURRED
-                            formats: [AUTO, WEBP, AVIF]
-                        )
-                    }
-                }
-            }
-        }
+ query BlogQuery {
+  blogs: allSanityPost(sort: { fields: publishedAt, order: DESC }, skip: 1) {
+   nodes {
+    title
+    id
+    excerpt
+    readingTimeInMinutes
+    slug {
+     current
     }
+    publishedAt(formatString: "MMM DD YYYY")
+    _rawBody
+    authors {
+     author {
+      name
+      position
+      image {
+       alt
+       asset {
+        gatsbyImageData(
+         width: 200
+         placeholder: BLURRED
+         formats: [AUTO, WEBP, AVIF]
+        )
+       }
+      }
+     }
+    }
+    mainImage {
+     alt
+     asset {
+      gatsbyImageData(
+       width: 900
+       placeholder: BLURRED
+       formats: [AUTO, WEBP, AVIF]
+      )
+     }
+    }
+   }
+  }
+  featuredBlog: allSanityPost(
+   sort: { fields: publishedAt, order: DESC }
+   limit: 1
+  ) {
+   nodes {
+    title
+    id
+    excerpt
+    readingTimeInMinutes
+    slug {
+     current
+    }
+    publishedAt(formatString: "MMM DD YYYY")
+    _rawBody
+    authors {
+     author {
+      name
+      position
+      image {
+       alt
+       asset {
+        gatsbyImageData(
+         width: 400
+         placeholder: BLURRED
+         formats: [AUTO, WEBP, AVIF]
+        )
+       }
+      }
+     }
+    }
+    mainImage {
+     alt
+     asset {
+      gatsbyImageData(
+       width: 900
+       placeholder: BLURRED
+       formats: [AUTO, WEBP, AVIF]
+      )
+     }
+    }
+   }
+  }
+ }
 `
